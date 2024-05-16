f35d0c2cb51ed91e79ca64579802dd7ea39976e5
Manasa-annasagaram <Manasa.Annasagaram@ibm.com>
2024-05-16 10:56:45 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.py
@@ -0,0 +1,6 @@
+username = input("enter your Name:")
+age= int(input("your age:"))
+year =str(( 2024 - age ) + 100 )
+
+
+print("your age will be 100 when: " + year)
---
+++ Java/Activities/Activity2.py
@@ -0,0 +1,6 @@
+usernum = int(input("Enter a number "))
+mod = usernum%2
+if mod > 0:
+    print("The number is odd")
+else :
+    print("The number is even")
